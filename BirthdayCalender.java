de3cfda917d46cf15bb180f8ea739fc6408a0314
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-11-25 18:21:48 -0800

    Add files via upload

--- BirthdayCalender.java
+++ BirthdayCalender.java
@@ -1,9 +1,50 @@
+/**
+ * 
+ * @author Karyme Flores
+ *
+ */
+import java.util.Scanner;
 
 public class BirthdayCalender {
 
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner kb = new Scanner(System.in); 
+		
+		String name2; 
+		String name1;
+		int day2; 
+		int day1;
+		int month2; 
+		int month1;
+		int year2; 
+		int year1; 
+		System.out.print("Please enter person one's name:   ");
+		name1 = kb.nextLine();
+		System.out.println("Please enter person one's birthday in numerical value: ");
+		day1 = kb.nextInt();
+		System.out.println("Please enter person one's birthmonth in numerical value:  "); 
+		month1 = kb.nextInt();
+		System.out.println("Please enter person one's yearin numerical value:");
+		year1 = kb.nextInt();
+		System.out.print("Please enter person two's name:   ");
+		name2 = kb.nextLine();
+		System.out.println("Please enter person two's birthday in numerical value: ");
+		day2 = kb.nextInt();
+		System.out.println("Please enter person two's birthmonth in numerical value:  "); 
+		month2 = kb.nextInt();
+		System.out.println("Please enter person two's year in numerical value:");
+		year2 = kb.nextInt();
+		
+		if(year1>year2)
+		{ 
+			System.out.print("Person 1 is younger than Person 2");
+		}
+		if(year1<year2)
+		{
+			System.out.print("Person 1 is older than Person 2");
+		}
 	}
 
 }
